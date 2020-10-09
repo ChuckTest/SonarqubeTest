c3422946477ca7c9ca8b8d1aff189689006edfee
Chuck Lu <chuck.lu@qq.com>
2020-10-09 10:15:25 +0800

    add test code which will be recognized as RSPEC-2223 "Non-constant static fields should not be visible" https://rules.sonarsource.com/csharp/type/Code%20Smell/RSPEC-2223

---
+++ SonarqubeTest/CustomHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SonarqubeTest
+{
+    public class CustomHelper
+    {
+        public static readonly List<string> MandatoryFields = new List<string>
+        {
+            "First name",
+            "Last name"
+        };
+    }
+}

c6e83ef437eb560e3e36def08bffc4cd638027a5
Chuck Lu <chuck.lu@qq.com>
2020-10-09 10:59:28 +0800

    add code which will be recognized as "Remove this unread private field '_clientRepository' or refactor the code to use its value." https://rules.sonarsource.com/csharp/tag/cwe/RSPEC-4487

---
+++ SonarqubeTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace SonarqubeTest
+{
+    public class Class1
+    {
+        private readonly string _test;
+
+        public Class1()
+        {
+            _test = "hello";
+        }
+    }
+}

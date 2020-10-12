12eb8000486ad1f6f421231b3751eba2870c8499
Chuck Lu <chuck.lu@qq.com>
2020-10-12 16:35:19 +0800

    use the organization name and project key from sonar-project.properties https://github.com/scottenriquez/sonarcloud-travis-ci-csharp/issues/2

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -2,7 +2,7 @@
 echo "Changing to /src directory..."
 cd SonarqubeTest
 echo "Executing MSBuild.exe begin command..."
-mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"scottenriquez-github" /k:"scottenriquez_sonarcloud-travis-ci-csharp" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"chucktest" /k:"ChuckTest_SonarqubeTest" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
 msbuild /p:Configuration=Release /t:Rebuild ../SonarqubeTest.sln
 echo "Executing MSBuild.exe end command..."

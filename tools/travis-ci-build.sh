2d4d3af5c040972b782adaae25d3abe481f0634e
Chuck Lu <chuck.lu@qq.com>
2020-10-12 15:40:52 +0800

    modify the build script

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 echo "Changing to /src directory..."
-cd src
+cd SonarqubeTest
 echo "Executing MSBuild.exe begin command..."
 mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"scottenriquez-github" /k:"scottenriquez_sonarcloud-travis-ci-csharp" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
-msbuild /p:Configuration=Release /t:Rebuild SonarCloud.Travis.Integration.sln
-echo "Running tests..."
-dotnet test SonarCloud.Travis.Integration.Tests/SonarCloud.Travis.Integration.Tests.csproj
+msbuild /p:Configuration=Release /t:Rebuild ../SonarqubeTest.sln
 echo "Executing MSBuild.exe end command..."
 mono ../tools/sonar/SonarScanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}

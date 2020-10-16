e47a407f326b3c8ee9ac3225a0995efdc2dbfc3e
Chuck Lu <chuck.lu@qq.com>
2020-10-16 10:19:47 +0800

    refactor code

--- Sonarqube/CustomHelper.cs
+++ Sonarqube/CustomHelper.cs
@@ -4,24 +4,10 @@ namespace Chuck.Sonarqube
 {
     public static class CustomHelper
     {
-        public static List<string> MandatoryFields = new List<string>
+        public static List<string> MandatoryFields { get; } = new List<string>
         {
             "First name",
             "Last name"
         };
-    }
-
-    public class Math
-    {
-        public static double Pi = 3.14;
-    }
-
-    public class Shape
-    {
-        public static Shape Empty = new EmptyShape();
-
-        private class EmptyShape : Shape
-        {
-        }
     }
 }

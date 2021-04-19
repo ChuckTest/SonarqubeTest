1676cb54715634981f709f96f4e22ab40b9be725
Chuck Lu <chucklu2015@outlook.com>
2021-04-19 17:50:41 +0800

    remove the old yml file

--- tools/travis-ci-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Changing to /src directory..."
-cd Sonarqube
-echo "Executing MSBuild.exe begin command..."
-mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"chucktest" /k:"ChuckTest_SonarqubeTest" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
-echo "Running build..."
-msbuild /p:Configuration=Release /t:Rebuild ../SonarqubeTest.sln
-echo "Executing MSBuild.exe end command..."
-mono ../tools/sonar/SonarScanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}
